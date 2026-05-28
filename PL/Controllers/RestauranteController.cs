28c38fed6fc5934846c4f9750d592308c54e2a99
evarias <evelynarias03@outlook.com>
2026-05-28 15:01:29 -0600

    Creacion de Metodos Update, Delete y GetById en BL, en ML creacion de propiedad para poder guardar la lista de Restaurantes, en PL se creo la vista y medos funcionales de GetAll y Delete

---
+++ PL/Controllers/RestauranteController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PL.Controllers
+{
+    public class RestauranteController : Controller
+    {
+        // GET: Restaurante
+        public ActionResult GetAll()
+        {
+            ML.Restaurante restaurante = new ML.Restaurante();
+            ML.Result result = BL.Restaurante.GetAll();
+            if (result.Correct)
+            {
+                restaurante.Restaurantes = result.Objects;
+            }
+            else
+            {
+                ViewBag.Error = result.Messages;
+            }
+
+
+            return View(restaurante);
+        }
+        [HttpPost]
+        public ActionResult Delete(int idRestaurante)
+        {
+            ML.Restaurante restaurante = new ML.Restaurante();
+            restaurante.IdRestaurante = idRestaurante;
+
+            ML.Result result = BL.Restaurante.Delete(restaurante.IdRestaurante);
+
+            if (result.Correct)
+            {
+                return RedirectToAction("GetAll", "Restaurante");
+            }
+            else
+            {
+                ViewBag.Error += result.Messages;
+                return RedirectToAction("GetAll", "Restaurante");
+            }
+        }
+    }
+}
